a5b9deff88bd215383d2099f58fc1d26b1f69922
Aaron Steinfeld <aaron@traceable.ai>
2021-08-12 17:23:26 -0400

    chore: more stuff

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 brew install zsh yq tmux git buf gdub tree
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -s $(pwd)/agnoster.zsh-theme $(pwd)/.oh-my-zsh/themes/agnoster.zsh-theme || echo "Skipping"
+
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
+sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
+sudo fc-cache -f -v
+sudo apt-get install -y fonts-powerline
+chsh -s /usr/bin/zsh $USERNAME
